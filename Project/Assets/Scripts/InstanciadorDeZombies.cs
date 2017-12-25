4426b02edeb37470eb1039229b7e99220fcf5e16
Hector Pulido <Hectorandrespp@gmail.com>
2017-12-25 01:41:54 -0500

    First Commit

---
+++ Project/Assets/Scripts/InstanciadorDeZombies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanciadorDeZombies : MonoBehaviour {
+
+
+    public int[] tiempos;
+
+    public GameObject Zombie;
+
+    void Start()
+    {
+        for (int i = 0; i < tiempos.Length; i++)
+        {
+            Invoke("InstanciarZombie", tiempos[i]);
+        }
+    }
+    void InstanciarZombie()
+    {
+        Instantiate(Zombie, transform.position, Zombie.transform.rotation);
+    }
+}
